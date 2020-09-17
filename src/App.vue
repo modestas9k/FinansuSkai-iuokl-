f600c0003afc523ebaf635085b37db9aa2e15187
Modestas Skirius <mode@Modestass-Mini.lan>
2020-09-17 21:03:16 +0300

    add navigation, notification

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
     <div id="app">
-        <div class="section">
-            <div id="nav">
-                <router-link to="/">Home</router-link>
-                |
-                <router-link to="/about">About</router-link>
-                |
-                <router-link to="/register">Register</router-link>
-            </div>
+        <Navigation />
+        <section class="section">
             <div class="container">
                 <router-view />
             </div>
-        </div>
+        </section>
     </div>
 </template>
+<script>
+import Navigation from "./components/Navigation";
+export default {
+    name: "app",
+    components: { Navigation },
+};
+</script>
 
 <style></style>
