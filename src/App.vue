930c1326f13b10090faa5935f7549fe484f3ef50
Modestas Skirius <mode@Modestass-Mini.lan>
2020-09-17 20:31:02 +0300

    add login, register

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div class="section">
+            <div id="nav">
+                <router-link to="/">Home</router-link>
+                |
+                <router-link to="/about">About</router-link>
+                |
+                <router-link to="/register">Register</router-link>
+            </div>
+            <div class="container">
+                <router-view />
+            </div>
+        </div>
     </div>
-    <router-view />
-  </div>
 </template>
 
-<style>
-</style>
+<style></style>
---
+++ src/views/Register.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="register container box max">
+        <h1 class="title">Registracija</h1>
+        <form>
+            <div class="field">
+                <label for="email" class="label">Elektroninis paštas</label>
+                <div class="control">
+                    <input
+                        class="input"
+                        type="text"
+                        name="email"
+                        id="email"
+                        placeholder="fredis@gmail.com"
+                        v-model="email"
+                    />
+                </div>
+            </div>
+            <div class="field">
+                <label for="password" class="label">Slaptažodis</label>
+                <div class="control">
+                    <input
+                        class="input"
+                        type="text"
+                        name="password"
+                        id="password"
+                        placeholder="ananasas123"
+                        v-model="password"
+                    />
+                </div>
+            </div>
+            <div class="field">
+                <div class="control">
+                    <button type="submit" class="button is-primary">
+                        Registruotis
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "register",
+    data() {
+        return {
+            email: "",
+            password: "",
+        };
+    },
+};
+</script>
+
+<style scoped>
+.max {
+    max-width: 600px;
+    margin: 0 auto;
+}
+</style>
