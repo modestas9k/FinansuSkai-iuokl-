9251eca8ef392338c5f4453a79d585d53617301a
Modestas Skirius <mode@Modestass-Mini.lan>
2020-09-21 11:06:06 +0300

    minor fixes login and register

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,61 +1,50 @@
 <template>
-    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <router-link class="navbar-item" to="/">
-                <h1 class="title">Student Register</h1>
-            </router-link>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/">
+        <h1 class="title">Finansu skaičiuoklė</h1>
+      </router-link>
 
-            <a
-                role="button"
-                class="navbar-burger burger"
-                aria-label="menu"
-                aria-expanded="false"
-                data-target="navbarBasicExample"
-            >
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start" v-if="loggedIn">
+        <router-link
+          v-for="link in links"
+          :key="link.name"
+          :to="link.url"
+          class="navbar-item"
+        >{{ link.name }}</router-link>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <button
+              v-if="loggedIn"
+              v-on:click="logout()"
+              class="button is-danger is-outlined"
+            >Logout</button>
+            <span v-else>
+              <router-link to="/register" class="button is-primary">Register</router-link>
+              <router-link to="/login" class="button is-light">Login</router-link>
+            </span>
+          </div>
         </div>
-
-        <div id="navbarBasicExample" class="navbar-menu">
-            <div class="navbar-start" v-if="loggedIn">
-                <router-link
-                    v-for="link in links"
-                    :key="link.name"
-                    :to="link.url"
-                    class="navbar-item"
-                >
-                    {{ link.name }}
-                </router-link>
-            </div>
-
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <button
-                            v-if="loggedIn"
-                            v-on:click="logout()"
-                            class="button is-danger is-outlined"
-                        >
-                            Logout
-                        </button>
-                        <span v-else>
-                            <router-link
-                                to="/register"
-                                class="button is-primary"
-                            >
-                                Register
-                            </router-link>
-                            <router-link to="/login" class="button is-light">
-                                Login
-                            </router-link>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -63,36 +52,36 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 export default {
-    data() {
-        return {
-            loggedIn: false,
-            email: "",
-            links: [
-                { name: "Students", url: "/" },
-                { name: "Groups", url: "/groups" },
-            ],
-        };
+  data() {
+    return {
+      loggedIn: false,
+      email: "",
+      links: [
+        { name: "Namai", url: "/" },
+        { name: "About", url: "/about" },
+      ],
+    };
+  },
+  methods: {
+    logout() {
+      this.loggedIn = false;
+      firebase.auth().signOut();
     },
-    methods: {
-        logout() {
-            this.loggedIn = false;
-            firebase.auth().signOut();
-        },
-    },
-    beforeMount() {
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                this.loggedIn = true;
-                this.email = firebase.auth().currentUser.email;
-            }
-        });
-    },
+  },
+  beforeMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.loggedIn = true;
+        this.email = firebase.auth().currentUser.email;
+      }
+    });
+  },
 };
 </script>
 
 <style scoped>
 .title {
-    color: white;
-    font-size: 1.5em;
+  color: white;
+  font-size: 1.5em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./servers/firebase";
 import "normalize.css";
 import "bulma/css/bulma.min.css";
-import "./servers/firebase";
 
 Vue.config.productionTip = false;
 
