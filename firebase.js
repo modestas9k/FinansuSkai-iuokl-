d2b4ad9549f70666bdd7efe7ee28ea68f6ce3e0c
Modestas Skirius <mode@Modestass-Mini.lan>
2020-09-28 15:55:12 +0300

    show firebase

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDL9OBD6h3cvjDA2AevwA7GJv84NUgeLYo",
+    authDomain: "finansuskaiciuokle.firebaseapp.com",
+    databaseURL: "https://finansuskaiciuokle.firebaseio.com",
+    projectId: "finansuskaiciuokle",
+    storageBucket: "finansuskaiciuokle.appspot.com",
+    messagingSenderId: "4568039936",
+    appId: "1:4568039936:web:7a3bf33b25740746a7cee5",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
