30e468709d407eac0aed8b599cf86dba3d07c8d2
Modestas Skirius <modestas.skirius@gmail.com>
2021-07-18 23:10:28 +0300

    better namig, change firebase structure

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -8,7 +8,7 @@
                         <div class="buttons">
                             <router-link
                                 :to="
-                                    `/product/${this.$route.params.type}/${produktas.id}/add`
+                                    `/users/${this.user}/${this.$route.params.type}/${this.product.id}/add`
                                 "
                                 class="button is-primary is-rounded"
                             >
@@ -35,11 +35,11 @@
                 </div>
 
                 <div class="box">
-                    <h1 class="title ">
-                        {{ this.produktas.pavadinimas }}
+                    <h1 class="product ">
+                        {{ this.product.title }}
                     </h1>
-                    <p class="subtitle ">
-                        {{ `${firstLastData(irasai)}` }}
+                    <p class="subproduct ">
+                        {{ `${firstLastData(records)}` }}
                     </p>
                     <div class="line"></div>
                     <div
@@ -62,18 +62,18 @@
                     <div class="line"></div>
                     <div class="columns is-mobile has-text-centered mb-1 mt-1">
                         <div class="column">
-                            <h5 class="subtitle has-text-weight-bold">
-                                {{ `${suma(irasai)} €` }}
+                            <h5 class="subproduct has-text-weight-bold">
+                                {{ `${total(records)} €` }}
                             </h5>
                         </div>
                         <div class="column">
-                            <h5 class="subtitle has-text-weight-bold">
-                                {{ `${kiekis(irasai)}` }}
+                            <h5 class="subproduct has-text-weight-bold">
+                                {{ `${quantity(records)}` }}
                             </h5>
                         </div>
                         <div class="column">
-                            <h5 class="subtitle has-text-weight-bold">
-                                {{ `${vidurkis(irasai)} €` }}
+                            <h5 class="subproduct has-text-weight-bold">
+                                {{ `${averagePrice(records)} €` }}
                             </h5>
                         </div>
                     </div>
@@ -95,7 +95,7 @@
                             </h5>
                         </header>
                         <div class="">
-                            <h5>{{ produktas.komentaras }}</h5>
+                            <h5>{{ product.comment }}</h5>
                         </div>
                         <div
                             class="modal"
@@ -104,7 +104,7 @@
                             <div class="modal-background"></div>
                             <div class="modal-card">
                                 <header class="modal-card-head">
-                                    <p class="modal-card-title">Užrašai</p>
+                                    <p class="modal-card-product">Užrašai</p>
                                     <button
                                         class="delete"
                                         aria-label="close"
@@ -113,7 +113,7 @@
                                 </header>
                                 <section class="modal-card-body">
                                     <textarea
-                                        v-model="produktas.komentaras"
+                                        v-model="product.comment"
                                         class="textarea"
                                     ></textarea>
                                 </section>
@@ -136,9 +136,12 @@
                     </div>
                 </div>
                 <div class="box">
-                    <h2 class="title">Istorija</h2>
+                    <h2 class="product">Istorija</h2>
                     <div class="line"></div>
                     <div class="columns has-text-centered is-mobile mt-1 mb-0">
+                        <div class="column">
+                            <h5 class="has-text-weight-bold">Suma</h5>
+                        </div>
                         <div class="column">
                             <h5 class="has-text-weight-bold">Kiekis</h5>
                         </div>
@@ -148,24 +151,85 @@
                         <div class="column">
                             <h5 class="has-text-weight-bold">Data</h5>
                         </div>
-                        <div class="column is-2"></div>
+                        <!-- <div class="column is-2"></div> -->
                     </div>
                     <div class="line"></div>
                     <div
-                        class="columns has-text-centered is-mobile mt-1"
-                        v-for="list in irasai"
-                        :key="list.id"
+                        class=""
+                        v-for="list in records"
+                        :key="list.id" 
                     >
-                        <div class="column ">
-                            <h5>{{ list.kiekis }}</h5>
+                    <div class="columns has-text-centered is-mobile mt-1" 
+                        @click="deleteProductRecord = true">
+                        <div class="column " >
+                            <h5>{{ `${list.total} €`}}</h5>
                         </div>
                         <div class="column ">
-                            <h5>{{ `${list.kaina} €` }}</h5>
+                            <h5>{{ list.quantity }}</h5>
                         </div>
                         <div class="column ">
-                            <h5>{{ list.data }}</h5>
+                            <h5>{{ `${list.averagePrice} €` }}</h5>
                         </div>
-                        <div class="column is-2">
+                        <div class="column ">
+                            <h5>{{ list.date }}</h5>
+                        </div>
+                    </div>
+                        <!-- <div
+                            class="modal"
+                            :class="{ 'is-active': deleteProductRecord == true }"
+                        >
+                            <div class="modal-background"></div>
+                            <div class="modal-card">
+                                <header class="modal-card-head">
+                                    <p class="modal-card-product">Ar norite ištrinti šį įraša?</p>
+                                    <button
+                                        class="delete"
+                                        aria-label="close"
+                                        @click="deleteProductRecord = false"
+                                    ></button>
+                                </header>
+                                
+                                <footer class="modal-card-foot">
+                                    <button
+                                        class="button is-primary is-rounded"
+                                        @click="deleteRecordsItem(list.id)"
+                                    >
+                                        Taip
+                                    </button>
+                                    <button
+                                        class="button is-primary is-outlined is-rounded"
+                                        @click="deleteProductRecord = false"
+                                    >
+                                        Ne
+                                    </button>
+                                </footer>
+                            </div>
+                        </div> -->
+                        <div class="modal"
+                        :class="{ 'is-active' : deleteProductRecord }">
+                            <div class="modal-background" @click="deleteProductRecord = false"></div>
+                            <div class="modal-content box">
+                                <p class="mb-2">Ar norite ištrinti šį įraša?</p>
+                                <button
+                                    class="button is-primary is-rounded"
+                                    @click="deleteRecordsItem(list.id)"
+                                >
+                                Taip
+                                </button>
+                                <button
+                                    class="button is-primary is-outlined is-rounded"
+                                    @click="deleteProductRecord = false"
+                                >
+                                    Ne
+                                </button>
+                            </div>
+                            <button 
+                                class="modal-close is-large" 
+                                aria-label="close" 
+                                @click="deleteProductRecord = false">
+                            </button>
+                        </div>
+                        <!-- <div class="column is-2">
                             <button
                                 class="button is-danger rounded"
                                 v-on:click="deleteStoryItem(list.id)"
@@ -174,7 +238,7 @@
                                     <i class="zmdi zmdi-close"></i>
                                 </span>
                             </button>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
@@ -184,51 +248,68 @@
 
 <script>
 import firebase from "firebase/app";
+import "firebase/auth";
 import "firebase/firestore";
 export default {
     name: "product",
 
     data() {
         return {
-            produktas: {},
-            irasai: [],
+            product: {},
+            records: [],
             showModal: false,
+            deleteProductRecord: false,
+            user: firebase.auth().currentUser.uid,
         };
     },
     methods: {
-        suma(array) {
+        total(array) {
             let answer = 0;
             array.forEach((obj) => {
-                answer += obj.kiekis * obj.kaina;
+                if(obj.averagePrice != 0 && obj.quantity != 0) {
+                    answer += obj.quantity * obj.averagePrice;
+                } else {
+                    answer += obj.total;
+                }
             });
             return answer;
         },
-        kiekis(array) {
+        quantity(array) {
             let answer = 0;
             array.forEach((obj) => {
-                answer += obj.kiekis;
+                if(obj.total != 0 && obj.averagePrice != 0) {
+                    answer += obj.total / obj.averagePrice;
+                } else {
+                    answer += obj.quantity;
+                }
             });
             return answer;
         },
         firstLastData(array) {
             if (array.length != 0) {
                 let first = array[0].data;
-                const lastfromArray = array.length - 1;
-                let last = array[lastfromArray].data;
+                const lastFromArray = array.length - 1;
+                let last = array[lastFromArray].data;
                 return first + " / " + last;
             } else {
                 return "Čia matysite informacija kai pridėsite įrašus";
             }
         },
-        vidurkis(array) {
+        averagePrice(array) {
             if (array != "") {
                 let number = 0;
-                let spliter = 0;
+                let splitter = 0;
                 array.forEach((obj) => {
-                    number += obj.kaina;
-                    spliter += 1;
+                    if(obj.averagePrice != 0) {
+                        number += obj.averagePrice;
+                        splitter += 1;
+                    } 
+                    else if(obj.total != 0 && obj.quantity != 0) {
+                        number += obj.total / obj.quantity;
+                        splitter += 1;
+                    }
                 });
-                let answer = number / spliter;
+                let answer = number / splitter;
                 return answer.toFixed(2);
             } else {
                 return 0;
@@ -237,6 +318,8 @@ export default {
         deleteItem() {
             firebase
                 .firestore()
+                .collection("users")
+                .doc(this.user)
                 .collection(this.$route.params.type)
                 .doc(this.$route.params.id)
                 .delete()
@@ -248,18 +331,21 @@ export default {
                     console.error("Error removing document: ", error);
                 });
         },
-        deleteStoryItem(id) {
+        deleteRecordsItem(id) {
             firebase
                 .firestore()
+                .collection("users")
+                .doc(this.user)
                 .collection(this.$route.params.type)
                 .doc(this.$route.params.id)
-                .collection("irasai")
+                .collection("records")
                 .doc(id)
                 .delete()
                 .then(() => {
-                    this.irasai = this.irasai.filter(
-                        (irasas) => irasas.id !== id
+                    this.records = this.records.filter(
+                        (record) => record.id !== id
                     );
+                    this.deleteProductRecord = false;
                     console.log("Document successfully deleted!");
                 })
                 .catch((error) => {
@@ -269,10 +355,12 @@ export default {
         update() {
             firebase
                 .firestore()
+                .collection("users")
+                .doc(this.user)
                 .collection(this.$route.params.type)
                 .doc(this.$route.params.id)
                 .update({
-                    komentaras: this.produktas.komentaras,
+                    comment: this.product.comment,
                 })
                 .then(() => {
                     this.showModal = false;
@@ -285,36 +373,67 @@ export default {
     beforeMount() {
         firebase
             .firestore()
+            .collection("users")
+            .doc(this.user)
             .collection(this.$route.params.type)
             .doc(this.$route.params.id)
             .get()
             .then((doc) => {
-                this.produktas = {
+                this.product = {
                     id: doc.id,
-                    pavadinimas: doc.data().pavadinimas,
-                    komentaras: doc.data().komentaras,
-                    uid: doc.data().uid,
+                    title: doc.data().title,
+                    comment: doc.data().comment,
                 };
-                firebase
-                    .firestore()
-                    .collection(this.$route.params.type)
-                    .doc(this.$route.params.id)
-                    .collection("irasai")
-                    .get()
-                    .then((doc) => {
-                        doc.docs.forEach((prod) => {
-                            this.irasai.push({
-                                id: prod.id,
-                                kiekis: Number(prod.data().kiekis),
-                                kaina: Number(prod.data().kaina),
-                                data: prod.data().data,
-                            });
-                        });
-                        this.irasai.sort((a, b) => {
-                            return new Date(a.data) - new Date(b.data);
-                        });
-                    });
+
+                // doc.records.forEach((record) => {
+                //     this.records.push({
+                //         date: record.data().date,
+                //         quantity: record.data().quantity,
+                //         total: record.data().total,
+                //     })
+                // })
+                // firebase
+                //     .firestore()
+                //     .collection(this.$route.params.type)
+                //     .doc(this.$route.params.id)
+                //     .collection("records")
+                //     .get()
+                //     .then((doc) => {
+                //         doc.docs.forEach((prod) => {
+                //             this.records.push({
+                //                 id: prod.id,
+                //                 kiekis: Number(prod.data().kiekis),
+                //                 kaina: Number(prod.data().kaina),
+                //                 data: prod.data().data,
+                //             });
+                //         });
+                //         this.records.sort((a, b) => {
+                //             return new Date(a.data) - new Date(b.data);
+                //         });
+                //     });
             });
+        firebase
+            .firestore()
+            .collection("users")
+            .doc(this.user)
+            .collection(this.$route.params.type)
+            .doc(this.$route.params.id)
+            .collection("records")
+            .get()
+            .then((doc) => {
+                doc.forEach((record) => {
+                    this.records.push({
+                        id: record.id,
+                        date: record.data().date,
+                        averagePrice: Number(record.data().averagePrice),
+                        quantity: Number(record.data().quantity),
+                        total: Number(record.data().total),
+                    })
+                })
+                this.records.sort((a, b) => {
+                    return new Date(a.data) - new Date(b.data);
+                });
+            })
     },
 };
 </script>
